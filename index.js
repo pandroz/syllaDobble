5a0ccd17be189c6f27bf08806ff7759ce65c4e70
pandroz <combi.alessandro.a@gmail.com>
2024-11-08 01:13:50 +0100

    [v. 0.2.0] Complete reword of the application to make it a decent nodejs app

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+import path from 'path';
+import _ from 'lodash';
+import { fileURLToPath } from 'url';
+
+import { openModal } from "./src/syllaDobbleScripts.js";
+
+
+const app = express();
+app.set('view engine', 'ejs');
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+console.log(__dirname);
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("/", (req, res) => {
+    res.render('./syllaDobble.ejs', { _: _ });
+})
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log("server running on port 3000");
+})

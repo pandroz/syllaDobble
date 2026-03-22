a9099dd527f0f789343406d73084fbb63dd246a5
pandroz <combi.alessandro.a@gmail.com>
2026-03-22 23:34:37 +0100

    [0.4.9] Cleared app.js file

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const path = require('path');
-const _ = require('lodash');
-const { fileURLToPath } = require('url');
-
-// import * as mgmt from "./src/managementScripts.js";
-
-
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -13,16 +7,6 @@ app.set('views', 'views');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-// const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-// const __dirname = path.dirname(__filename); // get the name of the directory
-// console.log(__dirname);
-
-// app.use(express.static(path.join(__dirname, "./public")));
-// app.use(express.json());
-
-
 
 
 // ROUTES
@@ -37,70 +21,4 @@ app.use('/data', dataRetrieverRoutes);
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
-})
-
-
-// // PAGES
-// app.get("/manage", (req, res) => {
-//     res.render('./pages/manageCards.ejs', { getGroupingsNames: mgmt.getGroupingsNames, _: _ })
-// })
-
-
-// // GET
-
-// app.get("/get-groupings", (req, res) => {
-    // const groupings = mgmt.getGroupings();
-//     res.json(groupings);
-// });
-
-
-// // POSTS
-
-// app.post("/get-groups", async (req, res) => {
-//     const groups = await mgmt.getGroups(req.body.groupingId);
-//     res.json(groups);
-// })
-
-// app.post('/add-new-grouping', async (req, res) => {
-//     const newGroupingName = req.body.groupName;
-//     const result = await mgmt.addNewGrouping(newGroupingName);
-
-//     if (result.success) {
-//         res.json({ success: true, message: 'Item added successfully', newId: result.newId });
-//     } else {
-//         res.status(500).json({ success: false, error: 'Failed to add item' });
-//     }
-// });
-
-// app.post('/save-grouping', async (req, res) => {
-//     const body = req.body;
-//     const success = await mgmt.saveGrouping(body);
-
-//     if (success) {
-//         res.json({ success: true, message: 'Item saved successfully' });
-//     } else {
-//         res.status(500).json({ success: false, error: 'Failed to add item' });
-//     }
-// });
-
-// app.delete('/delete-grouping', async (req, res) => {
-//     const groupingId = req.body.groupingId;
-//     const success = await mgmt.deleteGrouping(groupingId);
-
-//     if (success) {
-//         res.json({ success: true, message: 'Grouping deleted successfully' });
-//     } else {
-//         res.status(500).json({ success: false, error: 'Failed to delete grouping' });
-//     }
-// })
-
-// app.delete('/delete-group', async (req, res) => {
-//     const data = req.body;
-//     const result = await mgmt.deleteGroup(data);
-
-//     if (result.success) {
-//         res.json({ success: true, message: 'Group deleted successfully', groups: result.groups })
-//     } else {
-//         res.status(500).json({ success: false, error: 'Failed to delete group: ' + result.error })
-//     }
-// })
+});
